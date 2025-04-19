dd88c93351814833630390e2b21702572ada70e9
gregoriosiravo <gregoriosiravo@outlook.com>
2025-04-19 22:45:11 +0200

    added monitorCurrency

---
+++ src/services/monitorCurrency.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import TelegramBot from 'node-telegram-bot-api';
+import { User } from '../models/User';
+import { getCurrencyRate } from './currencyService';
+
+
+export const monitorCurrencyRates = async (bot: TelegramBot) => {
+  
+  const users = await getAllUsersWithTrackedCurrencies(); 
+
+  for (const user of users) {
+    if (user?.trackedCurrencies.length > 0) {
+      for (const trackedCurrency of user.trackedCurrencies) {
+        const { currency, goal } = trackedCurrency;
+        const currentRate = await getCurrencyRate(currency);
+
+        if (currentRate < goal) {
+          bot.sendMessage(
+            user.chatId,
+            `⚠️ Attenzione! Il tasso di cambio di ${currency} è sceso sotto la tua soglia di ${goal}. Attuale tasso: ${currentRate}`
+          );
+        }
+      }
+    }
+  }
+};
+
+const getAllUsersWithTrackedCurrencies = async () => {
+    try {
+     const users = await User.find({ 'trackedCurrencies.0': { $exists: true } });
+      return users;
+    } catch (error) {
+      console.error('❌ Error retrieving users with tracked currencies:', error);
+      return [];
+    }
+  };
